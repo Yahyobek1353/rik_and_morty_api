3f97a88998575e41dccbb1a1406f46452c68dd21
KARIMZHANOV <karimzanovumar@gmail.com>
2024-02-29 19:42:01 +0300

    v3

--- app/src/main/java/com/myself223/mvvm/MainActivity.kt
+++ app/src/main/java/com/myself223/mvvm/MainActivity.kt
@@ -3,10 +3,12 @@ package com.myself223.mvvm
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.renderscript.ScriptGroup.Binding
+import android.text.style.CharacterStyle
 import com.google.android.material.tabs.TabLayout
 import com.myself223.mvvm.databinding.ActivityMainBinding
 import com.myself223.mvvm.databinding.FragmentCharactersBinding
 
+@Suppress("DEPRECATION")
 class MainActivity : AppCompatActivity() {
     private val fragList = listOf(fragmentManager.fragments.size)
     private val binding: ActivityMainBinding by lazy {
@@ -15,6 +17,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
         binding.tbL.addOnTabSelectedListener(object :TabLayout.OnTabSelectedListener{
             override fun onTabSelected(tab: TabLayout.Tab?) {
 
